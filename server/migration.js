166994709a492e675fdbc61d82d3eec8a6a0b51e
Kosei Yamasaki <koseiyamasaki@Koseis-MBP.katch.ne.jp>
2021-08-02 13:43:38 +0900

    Update sequelize settings #3

--- server/migration.js
+++ server/migration.js
@@ -3,11 +3,11 @@ import * as data from "./sample-data.js";
 
 await sequelize.sync({ force: true });
 for (const { name, image, map } of data.restaurants) {
-  await Restaurant.create( { name, image, map });
+  await Restaurant.create({ name, image, map });
 }
 for (const { sub, nickname } of data.users) {
-  await User.create( { sub, nickname });
+  await User.create({ sub, nickname });
 }
 for (const { title, comment, userId, restaurantId } of data.reviews) {
-  await Review.create( { title, comment, userId, restaurantId });
+  await Review.create({ title, comment, userId, restaurantId });
 }
